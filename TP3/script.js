83d7f33286f15e9834983dae24d529a1584d8fd1
NOO3AD <diop97tidiane@gmail.com>
2022-02-22 16:07:26 +0000

    projetJavascript

---
+++ TP3/script.js
@@ -0,0 +1,6 @@
+const lmdp = document.getElementById('lmdp');
+const clM = document.getElementById('clM');
+const clm = document.getElementById('clm');
+const cn = document.getElementById('cn');
+const cs = document.getElementById('cs');
+const 
